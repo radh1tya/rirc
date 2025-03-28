4dfe1a6dab3c16a88ccb890e808e3202333db347
radhitya <radhitya@temanbsd.com>
2025-03-28 20:02:14 +0700

    now

--- makefile
+++ makefile
@@ -3,9 +3,9 @@
 NAME = rirc
 VERSION = 0.0
 
-SIC_CFLAGS = ${CFLAGS}
-SIC_LDFLAGS = ${LDFLAGS}
-SIC_CPPFLAGS = ${LDFLAGS} -DVERSION=\"${VERSION}\" -D_GNU_SOURCE
+RIRC_CFLAGS = ${CFLAGS}
+RIRC_LDFLAGS = ${LDFLAGS}
+RIRC_CPPFLAGS = ${LDFLAGS} -DVERSION=\"${VERSION}\" -D_GNU_SOURCE
 
 BIN = rirc
 SRC = ${BIN:=.c}
@@ -16,10 +16,10 @@ ALL = ${BIN}
 ${BIN}: ${@:=.o}
 
 .o:
-	${CC} -o $@ $< ${SIC_LDFLAGS}
+	${CC} -o $@ $< ${RIRC_LDFLAGS}
 
 .c.o:
-	${CC} -c ${SIC_CFLAGS} ${SIC_CPPFLAGS} -o $@ -c $<
+	${CC} -c ${RIRC_CFLAGS} ${RIRC_CPPFLAGS} -o $@ -c $<
 
 clean:
 	rm -f ${BIN} ${OBJ}
--- rirc.c
+++ rirc.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 typedef struct Message {
 	char content[256];
@@ -16,10 +20,58 @@ typedef struct Info {
 
 typedef struct Server {
 	char domain[50];
+	char str_port[5];
 	int port;
 } Server;
 
 int
 main(void)
 {
+	Message *message = malloc(sizeof(Message));
+	if (message == NULL) {
+		fprintf(stderr, "message *message memory allocation failed\n");
+		return 1;
+	}
+
+	Info *info = malloc(sizeof(Info));
+	if (info == NULL) {
+		fprintf(stderr, "Info *info memory allocation failed\n");
+		return 1;
+	}
+
+	Server *server = malloc(sizeof(Server));
+	if (server == NULL) {
+		fprintf(stderr, "Server *server memory allocation failed\n");
+		return 1;
+	}
+	
+	fprintf(stdout, "rirc - a nothing\n");
+
+	fprintf(stdout, "server: \n");
+	fgets(server->domain, 50, stdin);
+	server->domain[strcspn(server->domain, "\n")] = '\0';
+
+	fprintf(stdout, "port: \n");
+	fgets(server->str_port, 5, stdin);
+	server->str_port[strcspn(server->str_port, "\n")] = '\0';	
+	server->port = strtol(server->str_port, NULL, 10);
+
+	fprintf(stdout, "nickname: \n");
+	fgets(info->nickname, sizeof(info->nickname), stdin);
+	info->nickname[strcspn(info->nickname, "\n")] = '\0';
+
+	fprintf(stdout, "username: \n");
+	fgets(info->username, sizeof(info->username), stdin);
+	info->username[strcspn(info->username, "\n")] = '\0';
+	
+	fprintf(stdout, "realname: \n");
+	fgets(info->realname, sizeof(info->realname), stdin);
+	info->realname[strcspn(info->realname, "\n")] = '\0';
+
+
+	/* fprintf(stdout, "%d", server->port); */
+
+	int status, valread, client_fd;
+
+	struct sockaddr_in server_addr;
 }
